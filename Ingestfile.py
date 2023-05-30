31dd3eaa30df8b520bb47f0534ec8d548f91b8e6
Tofag <103821367+Tofag-py@users.noreply.github.com>
2023-05-30 10:54:40 +0000

    from codespaces

---
+++ Ingestfile.py
@@ -0,0 +1,47 @@
+import os
+import wget
+import zipfile
+
+
+def make_destination():
+    # Create a folder named "files" in the current directory
+    folder_path = os.path.join(os.getcwd(), "files")
+    try:
+        os.makedirs(folder_path, exist_ok=True)
+        return folder_path
+    except OSError as e:
+        print(f"Failed to create destination folder: {folder_path}")
+        return None
+
+def get_files(url, destination):
+    if destination is None:
+        print("Destination folder not available.")
+        return
+
+    try:
+        # Check if the file already exists in the destination folder
+        file_name = url.split("/")[-1]
+        file_path = os.path.join(destination, file_name)
+        if os.path.exists(file_path):
+            print(f"File already exists: {file_path}")
+        else:
+            # Download the file using wget
+            file_path = wget.download(url, out=destination)
+            print(f"File downloaded successfully: {file_path}")
+
+        # Extract the file using zipfile
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(destination)
+        print("File successfully extracted")
+
+    except Exception as e:
+        print(f"Failed to download file from: {url}")
+        print(e)
+
+def download_local():
+    url = "https://ed-public-download.app.cloud.gov/downloads/Most-Recent-Cohorts-Institution_04192023.zip"
+    
+    destination = make_destination()
+    get_files(url, destination)
+
+download_local()
